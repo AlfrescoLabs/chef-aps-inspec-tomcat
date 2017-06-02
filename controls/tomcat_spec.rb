4da020447fc72daa57a492963c476d87a48a7d80
jferdousAl <juena.ferdous@alfresco.com>
2017-06-02 10:35:24 +0100

    fix for tomcat-activiti service name

--- controls/tomcat_spec.rb
+++ controls/tomcat_spec.rb
@@ -23,7 +23,7 @@ control 'aps-appserver-03' do
   impact 0.7
   desc 'Checks Tomcat-activiti service running'
 
-  describe service('Tomcat-activiti') do
+  describe service('tomcat-activiti') do
     it { should_not be_enabled }
     it { should_not be_running }
   end
